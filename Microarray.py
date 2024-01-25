3a862f94aa86241e959dd42ce3a1f8fdf151ad71
GabrielBorgesG <128339908+GabrielBorgesG@users.noreply.github.com>
2024-01-25 15:15:34 -0300

    Update Microarray.py

--- Microarray.py
+++ Microarray.py
@@ -46,44 +46,60 @@ print(X_treino)
 print(X_teste)
 print(Y_treino)
 print(Y_teste)
-'''
-# c. CLASSIFICADOR SVM
 
-clf = svm.SVC() # cria um modelo simples de SVM
-clf.fit(X, Y) # treina o SVM nos pares de entrada X e Y
-# para predizermos a classe de uma amostra desconhecida a, usamos o comando abaixo, onde a é o vetor com a expressão gênica da amostra
-# clf.predict(a)
-prediction = clf.predict(X)
-print(prediction) # mostra as classes preditas para X pelo modelo em clf. Como esse resultado se compara às classes reais em Y?
-# para usar o kernel trick devemos passar o kernel como parâmetro na criação do SVM. Um kernel popular é o rbf (Radial Basis Function)
-# rbf_svc = svm.SVC(kernel='rbf')
-# rbf_svc.fit(X, Y)
+Tcancer = cm[0, 0]
+Fcancer = cm[1, 0]
+Tnormal = cm[1, 1]
+Fnormal = cm[0, 1]
+
+# Novo ----------------------------------------------------------------------------------------------------------------
+kmeans_clustering = KMeans(n_clusters=2, random_state=0).fit(X) # cria um modelo simples de k-means com 2 clusters, obtido dos dados em X
+clusters = kmeans_clustering.predict(X_teste)
+#print(clusters)
 
 # d, MÉTRICAS
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
-print(confusao)
-print(acuracia)
-print(sensibilidade)
-print(especificidade)
-print(f1_score)
+print("Matrix de confusão: ")
+print(cm)
+
+# Com função
+acuracia = accuracy_score(Y_teste, prediction)
+print("Acurácia: {}\n".format(acuracia))
+
+# "Manual" para confirmar o uso correto da função
+acuracia2 = (Tcancer+Tnormal)/(Tcancer + Tnormal + Fcancer + Fnormal)
+#print(acuracia2)
+
+sensibilidade = Tcancer/(Tcancer + Fnormal)
+print("Sensibilidade: {}\n".format(sensibilidade))
+
+especificidade = Tnormal/(Tnormal + Fcancer)
+print("Especificidade: {}\n".format(especificidade))
+
+print("Escore F1: ")
+f1_score_macro = f1_score(Y_teste, prediction, average='macro')
+f1_score_micro = f1_score(Y_teste, prediction, average='micro')
+f1_score_weighted = f1_score(Y_teste, prediction, average='weighted')
+print("Macro: {} | Micro: {} | Weighted: {}".format(f1_score_macro, f1_score_micro, f1_score_weighted))
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+print("Escore Silhouette:  {}".format(silhouette_score(X_teste, kmeans_clustering.fit_predict(X_teste))))
 
 # e. utilize o método k-means para analisar o dataset de estudo (preparado no item 'a')
 
 # 2 Grupos
+print("Para 2 grupos:\n{} dentro do cluster 1".format(list(clusters).count(0)))
+print("{} dentro do cluster 2".format(list(clusters).count(1)))
 
-print(n_amostras_2a)
-print(n_amostras_2b)
-
-# 3 Grupos
-
-print(n_amostras_3a)
-print(n_amostras_3b)
-print(n_amostras_3c)
-
-# 4 Grupos
-
-print(n_amostras_4a)
-print(n_amostras_4b)
-print(n_amostras_4c)
-print(n_amostras_4d)
-'''
+# 3 a 5 Grupos
+for i in range(3, 5):
+    kmeans_clustering = KMeans(n_clusters=i, random_state=0).fit(X) # cria um modelo simples de k-means com 2 clusters, obtido dos dados em X
+    clusters = kmeans_clustering.predict(X_teste)
+    print("Para {} grupos:".format(i))
+    for j in range(i):     
+        print("{} dentro do cluster {}".format(list(clusters).count(j), j+1))
